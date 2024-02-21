636c8cb4a81f86bb5d7421103b1f64178e4022bd
StanSNS <stanimirsergev159@gmail.com>
2024-02-21 17:54:32 +0200

    authLogin/add oAuth2 authorization.

--- src/main/java/com/example/carmodels/config/SpringSecurityConfig.java
+++ src/main/java/com/example/carmodels/config/SpringSecurityConfig.java
@@ -15,9 +15,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import static com.example.carmodels.constants.RoleConst.BASIC_USER;
-
-
 @Configuration
 @EnableMethodSecurity
 public class SpringSecurityConfig implements WebMvcConfigurer {
@@ -44,7 +41,7 @@ public class SpringSecurityConfig implements WebMvcConfigurer {
      * This bean defines various security rules and permissions for different endpoints.
      *
      * @param http The HttpSecurity object to configure security settings.
-     * @return     The configured SecurityFilterChain bean.
+     * @return The configured SecurityFilterChain bean.
      * @throws Exception If an error occurs while configuring security.
      */
     @Bean
@@ -55,23 +52,12 @@ public class SpringSecurityConfig implements WebMvcConfigurer {
         // Define authorization rules for different request matchers
         http.authorizeHttpRequests((authorize) -> {
             // Define request matchers and permissions
-            authorize.requestMatchers(
-                    "/auth/**"
-            ).permitAll();
-            authorize.requestMatchers("/").authenticated();
-            authorize.requestMatchers("/").hasRole(BASIC_USER);
-            authorize.requestMatchers("/add").authenticated();
-            authorize.requestMatchers("/add").hasRole(BASIC_USER);
-            authorize.requestMatchers("/addmodel").authenticated();
-            authorize.requestMatchers("/addmodel").hasRole(BASIC_USER);
-            authorize.requestMatchers("/edit/{id}").authenticated();
-            authorize.requestMatchers("/edit/{id}").hasRole(BASIC_USER);
-            authorize.requestMatchers("/updatemodel").authenticated();
-            authorize.requestMatchers("/updatemodel").hasRole(BASIC_USER);
-            authorize.requestMatchers("/delete/{id}").authenticated();
-            authorize.requestMatchers("/delete/{id}").hasRole(BASIC_USER);
+            authorize.requestMatchers("/auth/**").permitAll();
             authorize.anyRequest().authenticated();
         });
+
+        http.oauth2Login(Customizer.withDefaults());
+
         // Enable basic authentication with default settings
         http.httpBasic(Customizer.withDefaults());
 
